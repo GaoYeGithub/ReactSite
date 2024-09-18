554f6710ea13215b530622acc2036eac0232e162
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2024-09-18 19:12:23 -0400

    Charka Templete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { ChakraProvider } from '@chakra-ui/react';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Carousel from './components/Carousel';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Navbar />
+        <Hero />
+        <Carousel />
+      </div>
+    </ChakraProvider>
   );
 }
 
